1f215f0cb1a5d285dbc275122cee04c5352aeda6
edu-barrera <barreracarloseduardo03@gmail.com>
2024-08-06 10:37:33 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8234e9a1-ce86-48d3-8c7b-66a3ca03f7df';
+export const MODAL_CLOSE_SEC = 2.5;
